af9b235c47303a266b99f1ee4a7b71481e187800
Евгений Толкач <tolkachevgeniy007@gmail.com>
2023-02-10 23:33:01 +0300

    доделал задание 2.2.

--- index.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const titleModal = document.querySelector('.modal__title');
-const modalForm = document.querySelector('.modal__form');
-const checkboxModal = document.querySelector('.modal__label_file');
-const fatCheckbox_Modal = document.querySelector('.modal__submit');
---
+++ lesson_2_2/index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const CamelCasetitleModal = document.querySelector('.modal__title');
+const CamelCasemodalForm = document.querySelector('.modal__form');
+const CamelCasecheckboxModal = document.querySelector('.modal__label_file');
+const CamelCasefatCheckbox_Modal = document.querySelector('.modal__submit');
