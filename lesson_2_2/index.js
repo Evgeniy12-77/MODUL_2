9262a678351f12265baae8a0294c76f74628c7f0
Евгений Толкач <tolkachevgeniy007@gmail.com>
2023-02-11 23:02:21 +0300

    начало нового задания 3

--- lesson_2_2/index.js
+++ lesson_2_2/index.js
@@ -1,6 +1,3 @@
 'use strict'
 
-const CamelCasetitleModal = document.querySelector('.modal__title');
-const CamelCasemodalForm = document.querySelector('.modal__form');
-const CamelCasecheckboxModal = document.querySelector('.modal__label_file');
-const CamelCasefatCheckbox_Modal = document.querySelector('.modal__submit');
+
