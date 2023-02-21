1e4d8992a6bd0db601719872ced54de7477619c3
Евгений Толкач <tolkachevgeniy007@gmail.com>
2023-02-21 22:54:07 +0300

    добавил нужные файлы для cms

---
+++ lesson_2_5_b cms/test 2_3/index_test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const obj_1 = {
+  "text": "Heading 1",
+  "element": "h1", // Корневой элемент.
+  "properties": "Balboa Light, 30px", // Третий столбец текста.
+  "usage": ["Product title (once on a page)", "Illustration headline"] // Четвёртый столбец текста. Каждый элемент - это дочерний узел.
+};
+
+
+function render(createElement) {
+  return createElement("div", { class: "container" }, [
+    createElement("p", { class: "my-awesome-class" }, "Some cool text")
+  ])
+};
+
+console.log(render(obj_1));
+
+
