7fd3ddcda6fbceb756a610e39464bb26d65e8373
Евгений Толкач <tolkachevgeniy007@gmail.com>
2023-02-28 15:39:32 +0300

    делаю cms

--- 2.7 cms/index.js
+++ 2.7 cms/index.js
@@ -1,4 +1,36 @@
 'use strict'
+
+const createRow = (obj) => {
+  const returnColumn = () => {
+  return `<tr class='trdel' id="table">
+  <td class="table__cell" type="number" name="id">${obj.id}</td>
+      <td class="table__cell table__cell_left table__cell_name"
+      data-id="24601654816512" type="text" name="name">
+      <span class="table__cell-id">id: 24601654816512</span>
+      ${obj.name}</td>
+      <td class="table__cell table__cell_left">${obj.category}</td>
+      <td class="table__cell">${obj.units}</td>
+      <td class="table__cell">${obj.count}</td>
+      <td class="table__cell">$${obj.price}</td>
+      <td class="table__cell">$${obj.count * obj.price}</td>
+      <td class="table__cell table__cell_btn-wrapper">
+        <button class="table__btn table__btn_pic"></button>
+        <button class="table__btn table__btn_edit"></button>
+        <button class="table__btn table__btn_del"></button>
+      </td>
+    </tr>`
+    };
+    const table = document.querySelector('.table__body');
+    const column = returnColumn();
+    table.insertAdjacentHTML('beforeend', column);
+  };
+
+  function renderArray(Array) {
+    for (let i = 0; i < Array.length; i++) {
+    createRow(Array[i]);
+    };
+    };
+
 const objGood = {
   id: 2,
   title: 'Телевизор DEXP',
@@ -34,10 +66,10 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   const formData = new FormData(e.target);
   const newProduct = ([Object.fromEntries(formData)]);
+  console.log(newProduct);
   renderArray(newProduct);
   closeModal();
 });
-
 
 
 const trDelete = () => {
@@ -46,41 +78,14 @@ tableBody.addEventListener('click', e => {
   if(target.closest('.table__btn_del')) {
     target.closest('.trdel').remove();
     console.log(tableBody);
-  }
-}
-);
-};
+  }; }
+  )};
 trDelete();
-
-
-const createRow = (obj) => {
-
-const returnColumn = () => {
-return `<tr class='trdel'> <td class="table__cell" type="id">${obj.id}</td>
-    <td class="table__cell table__cell_left table__cell_name" data-id="24601654816512">
-    <span class="table__cell-id">id: 24601654816512</span>${obj.title}
-    </td>
-    <td class="table__cell table__cell_left">${obj.category}</td>
-    <td class="table__cell">${obj.units}</td>
-    <td class="table__cell">${obj.count}</td>
-    <td class="table__cell">$${obj.price}</td>
-    <td class="table__cell">$${obj.count * obj.price}</td>
-    <td class="table__cell table__cell_btn-wrapper">
-      <button class="table__btn table__btn_pic"></button>
-      <button class="table__btn table__btn_edit"></button>
-      <button class="table__btn table__btn_del"></button>
-    </td>
-  </tr>`
-  };
-  const table = document.querySelector('.table__body');
-  const column = returnColumn();
-  table.insertAdjacentHTML('beforeend', column);
-};
 
 let goods = [
   {
     "id": 3,
-    "title": "Смартфон Xiaomi 11T 8/128GB",
+    "name": "Смартфон Xiaomi 11T 8/128GB",
     "price": 27000,
     "description": "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
     "category": "mobile-phone",
@@ -94,7 +99,7 @@ let goods = [
   },
   {
     "id": 4,
-    "title": "Радиоуправляемый автомобиль Cheetan",
+    "name": "Радиоуправляемый автомобиль Cheetan",
     "price": 4000,
     "description": "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
     "category": "toys",
@@ -108,7 +113,7 @@ let goods = [
   },
   {
     "id": 5,
-    "title": "ТВ приставка MECOOL KI",
+    "name": "ТВ приставка MECOOL KI",
     "price": 12400,
     "description": "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
     "category": "tv-box",
@@ -122,7 +127,7 @@ let goods = [
   },
   {
     "id": 6,
-    "title": "Витая пара PROConnect 01-0043-3-25",
+    "name": "Витая пара PROConnect 01-0043-3-25",
     "price": 22,
     "description": "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
     "category": "cables",
@@ -136,10 +141,5 @@ let goods = [
   }
 ];
 
-function renderArray(Array) {
-  for (let i = 0; i < Array.length; i++) {
-  createRow(Array[i]);
-  };
-  };
-  renderArray(goods);
+ renderArray(goods);
 
